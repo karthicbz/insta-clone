0073415a05ef425e9c542f73ea72bc847b1c9026
Karthick <pkarthicbz@live.in>
2023-05-16 10:56:09 +0530

    add phone image

--- src/components/login-page.jsx
+++ src/components/login-page.jsx
@@ -1,9 +1,14 @@
+import homePhones from '../assets/images/home-phones.png';
+
 const LoginPage = ()=>{
     return(<div>
         <div className="loginpage-header"></div>
         <div className="loginpage-main">
-            <div>
-                <img />
+            <div className='phones-section'>
+                <img src={homePhones} alt='iphone and android'/>
+            </div>
+            <div className='login-section'>
+                
             </div>
         </div>
         <div className="loginpage-footer"></div>
