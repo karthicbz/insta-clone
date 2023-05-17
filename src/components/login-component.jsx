c51ddd78049d880594e26a3c8d4280e239d95c9f
Karthick <pkarthicbz@live.in>
2023-05-17 09:54:46 +0530

    disable button when userid and password is empty

--- src/components/login-component.jsx
+++ src/components/login-component.jsx
@@ -1,7 +1,34 @@
 import '../App.css';
 import googleIcon from '../assets/images/google.png'
+import { useEffect, useState } from 'react';
 
 const LoginComponent = ()=>{
+    const [userId, setUserId] = useState('');
+    const [password, setPassword] = useState('');
+
+    const checkUserIdLength = (e)=>{
+        setUserId(e.target.value);
+    }
+
+    const checkPassLength = (e)=>{
+        setPassword(e.target.value);
+    }
+
+    useEffect(()=>{
+        const loginButton = document.querySelector('.login-button');
+        const passField = document.querySelector('.user-pass');
+        const userIdField = document.querySelector('.user-id');
+
+        if(passField.value.length <= 5 || userIdField.value.length <=0){
+            loginButton.setAttribute('style', 'background-color: rgb(121, 169, 121)');
+            loginButton.disabled = true;
+        }else{
+            loginButton.removeAttribute('style');
+            loginButton.disabled = false;
+        }
+    }, [password, userId])
+
+
     return(
         <div className="login-component">
             <div className="login-main">
@@ -9,8 +36,8 @@ const LoginComponent = ()=>{
                     <p>Instagram</p>
                 </div>
                 <div className="main">
-                    <input className="user-id" type="text" placeholder="Phone number, username, or email"/>
-                    <input className="user-pass" type="password" placeholder="Password"/>
+                    <input className="user-id" type="text" placeholder="Phone number, username, or email" value={userId} onChange={checkUserIdLength}/>
+                    <input className="user-pass" type="password" placeholder="Password" value={password} onChange={checkPassLength}/>
                     <button className='login-button'>Log in</button>
                     <div className="divider">
                         <hr className="divider-left"/>
@@ -19,6 +46,9 @@ const LoginComponent = ()=>{
                     </div>
                     <div className='google-login'>
                         <button><img src={googleIcon} alt="google icon"/>Log in with google</button>
+                    </div>
+                    <div className='login-err'>
+                        <p></p>
                     </div>
                 </div>
             </div>
